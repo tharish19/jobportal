a033e8eb6ca2e6bd80abeeb052e7aed0b92b0bb5
Naga Venkatesh Kata <nagavenkateshchowdary@gmail.com>
2020-05-24 17:12:10 +0530

    commit for dynamic class

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    @HostBinding('class') public cssClass = 'DefaultClass';
+
+    constructor(private router: Router) {}
+    ngOnInit() {
+        // this.router.events.filter(event => event instanceof NavigationEnd).subscribe((_event: NavigationEnd) => {
+        //     const body = document.getElementsByTagName('body')[0];
+        //     const modalBackdrop = document.getElementsByClassName('modal-backdrop')[0];
+        //     if (body.classList.contains('modal-open')) {
+        //       body.classList.remove('modal-open');
+        //       modalBackdrop.remove();
+        //     }
+            this.cssClass = 'DefaultClass';
+        //   });
+    }
+
 }
