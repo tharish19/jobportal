004530c618043dba303f0f28525c1f0b6332264c
Harish Tatini <tharish19@gmail.com>
2020-06-05 15:04:02 +0530

    Modified Project structure

--- UI/src/app/Interfaces/IJobRoles.ts
+++
@@ -1,4 +0,0 @@
-export interface IJobRoles {
-    jobRoleId: number;
-    jobRole: string;
-}
--- UI/src/app/Interfaces/JobStatus.ts
+++
@@ -1,6 +0,0 @@
-export class JobStatus {
-    jobID: number;
-    jobStatus: string;
-    appliedBy: string;
-    AppliedOn: Date;
-}
